aaa4cdf83b2426917545c1f69c29cd20258501c5
Chaitanya Kulkarni <chaitanyjk305@gmail.com>
2025-03-17 10:44:10 +0530

    init nuxt project with a simple backend 1 file

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+  <template>
+      <div>
+        <h1>About Nuxt.js</h1>
+        <p>Nuxt.js is a framework for building Vue.js applications.</p>
+        <nuxt-link to="/">Back to Home</nuxt-link>
+      </div>
+    </template>
+    
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+    <template>
+        <div>
+        <h1>Welcome to Nuxt.js</h1>
+        <p>This is a simple Nuxt.js demo.</p>
+        <nuxt-link to="/about">Go to About Page</nuxt-link>
+        </div>
+    </template>
+    
